5ccbd51dde9d2624ae45f2982e430e58114daeb3
Alex Wollenschlaeger <alexw@panix.com>
2015-02-04 15:12:57 +0000

    Initials commit

---
+++ test/sml/AddInstructionTest.java
@@ -0,0 +1,39 @@
+package sml;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class AddInstructionTest {
+
+	/*
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		String filename = "src/code.sml";
+		Machine m = new Machine();
+		Translator t = new Translator(filename);
+		t.readAndTranslate(m.getLabels(), m.getProg());
+	}
+	*/
+
+	@Test
+	public void AddInstructionCorrectlyAddsTwoOperands() {
+		Machine m = new Machine();
+		m.setRegisters(new Registers());
+		int s1 = 20;
+		int s2 = 21;
+		int r = 22;
+		int op1 = 3;
+		int op2 = 4;
+		String l1 = "f1";
+		m.getRegisters().setRegister(s1, op1);
+		m.getRegisters().setRegister(s2, op2);
+		Instruction ins = new AddInstruction(l1, r, s1, s2);
+		ins.execute(m);
+		int actual = m.getRegisters().getRegister(r);
+		assertEquals(op1 + op2, actual);
+	}
+
+}
