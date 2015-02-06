48fa4b519d6ad958bf7197f9ab91956bfc7e13aa
Alex Wollenschlaeger <alexw@panix.com>
2015-02-06 16:53:13 +0000

    First commit.

---
+++ src/sml/BnzInstruction.java
@@ -0,0 +1,36 @@
+package sml;
+
+/**
+ * This class ....
+ * 
+ * @author someone
+ */
+
+public class BnzInstruction extends Instruction {
+
+	private int op1;
+	private String label2;
+
+	public BnzInstruction(String label, String op) {
+		super(label, op);
+	}
+
+	public BnzInstruction(String label, int op1, String label2) {
+		this(label, "bnz");
+		this.op1 = op1;
+		this.label2 = label2;
+	}
+
+	@Override
+	public void execute(Machine m) {
+		if (m.getRegisters().getRegister(op1) != 0) {
+			m.setPc(m.getLabels().indexOf(label2));
+		}
+	}
+
+	@Override
+	public String toString() {
+		// TODO
+		return super.toString();
+	}
+}
