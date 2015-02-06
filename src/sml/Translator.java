df74f68890506c098311f61c8e63b19746ae2154
Alex Wollenschlaeger <alexw@panix.com>
2015-02-06 15:45:24 +0000

    Added cases for sub, mul, div, out

--- src/sml/Translator.java
+++ src/sml/Translator.java
@@ -88,6 +88,24 @@ public class Translator {
 			s1 = scanInt();
 			s2 = scanInt();
 			return new AddInstruction(label, r, s1, s2);
+		case "sub":
+			r = scanInt();
+			s1 = scanInt();
+			s2 = scanInt();
+			return new SubInstruction(label, r, s1, s2);
+		case "mul":
+			r = scanInt();
+			s1 = scanInt();
+			s2 = scanInt();
+			return new MulInstruction(label, r, s1, s2);
+		case "div":
+			r = scanInt();
+			s1 = scanInt();
+			s2 = scanInt();
+			return new DivInstruction(label, r, s1, s2);
+		case "out":
+			r = scanInt();
+			return new OutInstruction(label, r);
 		case "lin":
 			r = scanInt();
 			s1 = scanInt();
