5a8ae4e3214c3f59840d4f26069f2de68b03a5e7
Alex Wollenschlaeger <alexw@panix.com>
2015-02-07 12:21:15 +0000

    Changed variable names to match specification.

--- src/sml/Translator.java
+++ src/sml/Translator.java
@@ -77,7 +77,7 @@ public class Translator {
 		int s2;
 		int r;
 		int x;
-		String l2;
+		String L2;
 
 		if (line.equals(""))
 			return null;
@@ -105,16 +105,16 @@ public class Translator {
 			s2 = scanInt();
 			return new DivInstruction(label, r, s1, s2);
 		case "out":
-			r = scanInt();
-			return new OutInstruction(label, r);
+			s1 = scanInt();
+			return new OutInstruction(label, s1);
 		case "lin":
 			r = scanInt();
 			x = scanInt();
 			return new LinInstruction(label, r, x);
 		case "bnz":
 			s1 = scanInt();
-			l2 = scan();
-			return new BnzInstruction(label, s1, l2);
+			L2 = scan();
+			return new BnzInstruction(label, s1, L2);
 		}
 
 		// You will have to write code here for the other instructions.
